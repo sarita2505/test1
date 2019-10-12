8b1b9adb8232886ce234dd0b7bd7f05d38004e19
sarita <saritapanda1996@gmail.com>
2019-10-13 00:21:29 +0530

    commit 1

---
+++ src/main/java/KeygenEx.java
@@ -0,0 +1,19 @@
+public class KeygenEx {
+}
+/**
+ * 1.from the current project location open git bash
+ * 2.write git status command..it will show not a got repojectory.so
+ * we need to make the current project as git reprojectory by the following steps
+ * 3.git init==used to make current directory as git  repo.. and it gives a branch called master
+ * 4.currently all files are in working tree..we need to add it in git repo..
+ * 5.git add <file location>--after adding check the status..it shows the
+ * added files are in green color..means it is in the staging tree..amd it
+ * suggests us to commit the changes..so do the things below
+ * 6.commit -m "some message"--but for commiting git needs some identification
+ * like--git config user.email "saritapanda1996@gmail.com"
+ *      git config user.name "sarita"
+ *
+ *
+ *
+ *
+ */
